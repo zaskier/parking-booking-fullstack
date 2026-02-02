2a1cb1b3cf177cfb4eef91eb1ae74a3e4371bef1
wojciech-iskierka <wojciech.iskierka@ingka.com>
2026-02-02 18:56:38 +0100

    #1 fix: update midlleware auth paths

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,12 +1,5 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
-
-const protectedRoutes = ['/add-offer', '/account', '/booking']
-
-function isProtectedRoute(pathname: string): boolean {
-  return protectedRoutes.some((route) => pathname.startsWith(route))
-}
-
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
@@ -30,6 +23,7 @@ export async function updateSession(request: NextRequest) {
           cookiesToSet.forEach(({ name, value, options }) =>
             request.cookies.set(name, value),
           )
+
           supabaseResponse = NextResponse.next({
             request,
           })
@@ -45,11 +39,39 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
-  if (!user && isProtectedRoute(request.nextUrl.pathname)) {
-    const url = request.nextUrl.clone()
-    url.pathname = '/login'
-    return NextResponse.redirect(url)
+  const redirectAuthorisedUserRoutes = ['/sign-in', '/sign-up']
+  const redirectUnauthorisedUserRoutes = ['booking', 'about', 'add-offer']
+
+  if (!user) {
+    if (
+      redirectAuthorisedUserRoutes.some((route) =>
+        request.nextUrl.pathname.startsWith(`/${route}`),
+      )
+    ) {
+      const url = request.nextUrl.clone()
+      url.pathname = '/sign-in'
+      return NextResponse.redirect(url)
+    }
+  } else {
+    if (redirectUnauthorisedUserRoutes.includes(request.nextUrl.pathname)) {
+      const url = request.nextUrl.clone()
+      url.pathname = '/'
+      return NextResponse.redirect(url)
+    }
   }
+
+  // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
+  // creating a new response object with NextResponse.next() make sure to:
+  // 1. Pass the request in it, like so:
+  //    const myNewResponse = NextResponse.next({ request })
+  // 2. Copy over the cookies, like so:
+  //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
+  // 3. Change the myNewResponse object to fit your needs, but avoid changing
+  //    the cookies!
+  // 4. Finally:
+  //    return myNewResponse
+  // If this is not done, you may be causing the browser and server to go out
+  // of sync and terminate the user's session prematurely!
 
   return supabaseResponse
 }
